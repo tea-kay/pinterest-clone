0faf48e7873c2bf0b1f696bc7f08c36292fa45e9
Timothy Kim <tkim1202@gmail.com>
2017-11-23 18:15:00 +0900

    Connected jQuery, masonry, and bootstrap. Also got grids to work with.

---
+++ public/javascripts/jQuery.js
@@ -0,0 +1,10 @@
+$('.grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  // options
+});
